c19dc5ed9a2558f3beaadfcf83fba26ac312bf66
bschwarz <bschwarz@student.42vienna.com>
2025-07-22 15:36:39 +0200

    expand

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bschwarz <bschwarz@student.42vienna.com    +#+  +:+       +#+        */
+/*   By: bschwarz <bschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 10:56:49 by bschwarz          #+#    #+#             */
-/*   Updated: 2025/06/05 15:26:47 by bschwarz         ###   ########.fr       */
+/*   Updated: 2025/07/22 15:35:18 by bschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,19 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
+typedef struct s_dll
+{
+	int				content;
+	struct s_dll	*prev;
+	struct s_dll	*next;
+}	t_dll;
+
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+int		ft_isspace(int c);
 
 char	**ft_split(char const *s, char c);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
@@ -81,6 +89,18 @@ t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
+
+void	ft_dlladd_beginning(t_dll **tail, int value);
+void	ft_dlladd_end(t_dll **head, int value);
+void	ft_dlldeallocate(t_dll **tail, t_dll **head);
+t_dll	*ft_dllfind_node_rec(t_dll *node, int value);
+t_dll	*ft_dllfind_node(t_dll *tail, int value);
+void	ft_dllinit(t_dll **tail, t_dll **head, int value);
+void	ft_dllinsert_after(t_dll *node, int value);
+void	ft_dlloutput_backward(t_dll *head);
+void	ft_dlloutput_forward(t_dll *tail);
+void	ft_dllremove_node(t_dll *node);
+void	ft_dllreverse(t_dll **tail, t_dll **head);
 
 char	*get_next_line(int fd);
 
